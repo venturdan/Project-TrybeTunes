dbf9a515fa3f968617eb5aac3938f1e969c98b90
venturdan <venturdan@outlook.com.br>
2023-04-17 02:07:45 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
-import Search from './pages/Search';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
@@ -14,15 +13,14 @@ class App extends React.Component {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route exact path="/search" component={ Search } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
         <Route component={ NotFound } />
       </Switch>
     );
   }
 }
-
 export default App;
